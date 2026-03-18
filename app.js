5a073651a76f9cd101e207f11c4e0e0c72808bf1
Shivam Yadav <163038292+shivamyadav05@users.noreply.github.com>
2026-03-18 15:42:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello, Node.js is running successfully!");
+const http = require('http');
+ 
+const server = http.createServer((req, res) => {
+  res.end("There is in the worst of fortune the best chances for a happy change");
+});
+ 
+server.listen(3000, () => {
+  console.log("App running on port 3000");
+});
